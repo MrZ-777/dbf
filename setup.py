89dcee114d11927370933108c6dcf2aee42726f7
MrZ-777 <19917783+MrZ-777@users.noreply.github.com>
2025-03-23 21:12:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ data = dict(
         license='BSD License',
         description='Pure python package for reading/writing dBase, FoxPro, and Visual FoxPro .dbf files (including memos)',
         long_description=long_desc,
-        url='https://github.com/MrZ-777/dbf',
+        url='https://github.com/ethanfurman/dbf',
         packages=['dbf', ],
         package_data={
            'dbf' : [
